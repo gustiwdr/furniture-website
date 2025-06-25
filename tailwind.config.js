f7f06c0e1a4aa10e0c0c9dcd2d3c205dd1cf4de1
gustiwdr <gustiputuwulandari1409@gmail.com>
2025-06-25 13:58:14 +0700

    refactor: migrate to nextjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./index.html", "./**/*.html", "./**/*.{js,ts,jsx,tsx}"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
 	theme: {
 		extend: {
 			colors: {
-				primary: "#054c73",
-				lightbg: "#f2f5ff",
-				textdark: "#333333",
-				errored: "#ef4444",
+				primary: "var(--primary)",
+				textdark: "var(--text-dark)",
+				lightbg: "var(--light-bg)",
 			},
 			fontFamily: {
-				montserrat: ['"Montserrat"', "sans-serif"],
+				montserrat: ["Montserrat", "sans-serif"],
 			},
 			height: {
 				933: "933px",
@@ -19,10 +22,10 @@ module.exports = {
 				643: "643px",
 			},
 			borderRadius: {
-				"tl-12": "12px 0 0 0",
 				"tr-75": "0 75px 0 0",
-				"bl-75": "0 0 0 75px",
+				"tl-12": "12px 0 0 0",
 				"br-12": "0 0 12px 0",
+				"bl-75": "0 0 0 75px",
 			},
 		},
 	},
